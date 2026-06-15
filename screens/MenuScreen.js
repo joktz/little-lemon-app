99ef89fc9ecb7f3a20fcb03c27e79633faaedbfd
joktz <jonathankrtz@gmail.com>
2026-06-15 11:58:55 +0900

    Feat: Displayed menu items from fetch and styled according to exercise instructions.

--- screens/MenuScreen.js
+++ screens/MenuScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 // Menu fetching function
 
@@ -13,10 +13,12 @@ const MenuScreen = () => {
         const getMenu = async () => {
             try {
             const response = await fetch(
-                'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json'
+                'https://raw.githubusercontent.com/Meta-Mobile-Developer-PC/Working-With-Data-API/main/menu-items-by-category.json', {
+
+                }
             );
-            const json = await response.json();
-            setResponse(json);
+            const data = await response.json();
+            setResponse(data.menu);
             } catch (error) {
                 console.error(error);
             }
@@ -25,18 +27,48 @@ const MenuScreen = () => {
     }, []);
 
     return (
-        <View>
+        <View style={styles.container}>
             <Text style={styles.header}>MENU</Text>
-            <Text>{response ? 'Response found' : 'None found'}</Text>
+            {
+                response ? (
+                    <FlatList
+                        data={response}
+                        keyExtractor={(item) => item.id}
+                        renderItem={({ item }) => (
+                            <View style={styles.menuCard}>
+                                <Text style={styles.menuText}>{item.title}</Text>
+                                <Text style={styles.menuText}>${item.price}</Text>
+                            </View>    
+                        )}
+                    />
+                ) : (
+                    <Text>We're sorry! The menu isn't loading right now...</Text>
+                )
+            }
         </View>
     )
 };
 
 const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        backgroundColor: '#333333',
+    },
     header: {
         textAlign: 'center',
         fontSize: 24,
         alignItems: 'center',
+        color: 'yellow',
+    },
+    menuCard: {
+        padding: 16,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    menuText: {
+        color: 'yellow',
+        fontSize: 24,
     }
 })
 
