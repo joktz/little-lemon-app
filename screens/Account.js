6072834f948da646510b293a6e968d01efef3e55
joktz <jonathankrtz@gmail.com>
2026-06-18 12:23:27 +0900

    Feat: Added marketing and news switches with matching states.

--- screens/Account.js
+++ screens/Account.js
@@ -2,12 +2,25 @@ import React, {useState} from 'react';
 import {View, Text, StyleSheet, Switch} from 'react-native';
 
 const AccountScreen = () => {
+    const [push, setPush] = useState(false);
+    const [marketing, setMarketing] = useState(false);
+    const [news, setNews] = useState(false);
+
+
     return (
         <View style={styles.container}>
             <Text style={styles.header}>Account Preferences</Text>
             <View style={styles.settingCard}>
                 <Text style={styles.settingText}>Push notifications</Text>
-                <Switch />
+                <Switch value={push} onValueChange={setPush} />
+            </View>
+            <View style={styles.settingCard}>
+                <Text style={styles.settingText}>Marketing emails</Text>
+                <Switch value={marketing} onValueChange={setMarketing} />
+            </View>
+            <View style={styles.settingCard}>
+                <Text style={styles.settingText}>Latest news</Text>
+                <Switch value={news} onValueChange={setNews} />
             </View>
         </View>
     )
@@ -27,10 +40,11 @@ const styles = StyleSheet.create({
     settingCard: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        padding: 8
+        padding: 8,
+        margin: 16
     },
     settingText: {
-        fontSize: 16,
+        fontSize: 24,
         alignItems: 'center',
     }
 })
