64c99d0a0ea5d4e2818c210be3b63608b718c58f
joktz <jonathankrtz@gmail.com>
2026-06-22 10:39:17 +0900

    Feat: Build save customer button and started customer list.

--- screens/Customers.js
+++ screens/Customers.js
@@ -15,6 +15,12 @@ const CustomerScreen = () => {
                     placeholder={'Enter the customer name'}
                     style={styles.input}
                 />
+                <Pressable style={styles.button}>
+                    <Text style={styles.buttonText}>Save Customer</Text>
+                </Pressable>
+                <View style={styles.customerList}>
+                    <Text style={styles.listText}>Customers:</Text>
+                </View>
             </ScrollView>
         </KeyboardAvoidingView>
         
@@ -38,6 +44,23 @@ const styles = StyleSheet.create({
         padding: 8,
         margin: 16,
         textAlign: 'center',
+        fontSize: 16,
+    },
+    button: {
+        backgroundColor: 'green',
+        padding: 16,
+        alignItems: 'center',
+        margin: 8,
+    },
+    buttonText: {
+        color: 'white',
+        fontSize: '16',
+    },
+    customerList: {
+        margin: 8,
+    },
+    listText: {
+        fontSize: 16,
     }
 })
 
